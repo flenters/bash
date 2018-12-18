8c28df2c10e1a53f7d93e9d0579ada0c9a912652
flenters <flenters@gmail.com>
2018-12-18 07:35:19 +0200

    Update checkports.sh

--- checkports.sh
+++ checkports.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 wipe="\033[1m\033[0m"
 yellow='\E[1;36m'
-nc -z -w1 ldaphosts.rmb.co.za 389
+nc -z -w1 hostname port
 ldapresult=$?
 if [ "$ldapresult" != 0 ]; then
-        echo -e "Port 389  is closed to ldaphosts.rmb.co.za"
+        echo -e "Port 389  is closed to hostname"
 else
-        echo -e "Port 389  is${yellow} open${wipe} to ldaphosts.rmb.co.za"
+        echo -e "Port 389  is${yellow} open${wipe} to hostname"
 fi
